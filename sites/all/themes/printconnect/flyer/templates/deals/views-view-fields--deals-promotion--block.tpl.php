ebd5b66f261d7d5d099e382c81b4d983fe138ac6
mohamed mouhsni <mohamed.m@oneoweb.com>
2014-08-15 18:36:14 +0100

    bloc deals

--- sites/all/themes/printconnect/flyer/templates/deals/views-view-fields--deals-promotion--block.tpl.php
+++ sites/all/themes/printconnect/flyer/templates/deals/views-view-fields--deals-promotion--block.tpl.php
@@ -2,14 +2,15 @@
 
 
 <?php if(!empty($fields['field_vignette_deal']->content)):?>	
-
-        <?php print $fields['field_vignette_deal']->content; ?>
+    <div class="boxing">
+        <div class="img"><?php print $fields['field_vignette_deal']->content; ?></div>
 
         <?php print $fields['body']->content; ?>
         
-    <p id="prixdeal">
-        <span class="pricedeal"> <?php print $fields['field_prix_deal']->content ?></span><span class="euro"> €</span> au lieu de <span class="pricedeal"><?php print $fields['field_prix_promotion']->content; ?></span><span class="euro"> €</span>
-    </p>
+        <p id="prixdeal">
+            <span class="newpricedeal"> <?php print $fields['field_prix_deal']->content ?></span><span class="neweuro"> €</span> au lieu de <span class="pricedeal"><?php print $fields['field_prix_promotion']->content; ?></span><span class="euro"> €</span>
+        </p>
+    </div>
     
      <?php print $fields['field_temps_deal']->content; ?>
     

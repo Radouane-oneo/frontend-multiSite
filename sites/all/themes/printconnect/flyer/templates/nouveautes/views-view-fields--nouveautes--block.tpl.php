605f4ae0d028bb0d7fe0d4c9cea67dc3f1074c97
oneonabil <nabil.b@oneoweb.com>
2014-08-25 18:35:18 +0000

    update front final

--- sites/all/themes/printconnect/flyer/templates/nouveautes/views-view-fields--nouveautes--block.tpl.php
+++ sites/all/themes/printconnect/flyer/templates/nouveautes/views-view-fields--nouveautes--block.tpl.php
@@ -6,7 +6,7 @@
     <a href="<?php print $fields['field_lien_nouveaute']->content; ?>">
 
         <?php print $fields['field_vignette_nouveaute']->content; ?>
-
+        <h2><?php print $fields['title']->content; ?></h2>
     </a>
    
  <?php endif;?>

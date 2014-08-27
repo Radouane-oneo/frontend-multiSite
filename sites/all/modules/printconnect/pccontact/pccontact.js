88900bc1d07c0913ac4c7e8565edbe1501b93e51
oneonabil <nabil.b@oneoweb.com>
2014-08-27 15:35:45 +0000

    send mail

--- sites/all/modules/printconnect/pccontact/pccontact.js
+++ sites/all/modules/printconnect/pccontact/pccontact.js
@@ -1,22 +1,5 @@
-//// JavaScript Document
-//
-//
-//function validate(email, phone) {
-//
-//    if (emailReg.test(email)) {
-//        jQuery(document).find('#pccontact_general_form #edit-actions')
-//		.append("<p class='errorForm'>Email Invalide</p>").hide().fadeIn().show();
-//
-//        return false;
-//    } else if (phoneReg(phone)) {
-//
-//        jQuery(document).find('#pccontact_general_form #edit-actions')
-//		.append("<p class='errorForm'>Télephone Invalide</p>").hide().fadeIn().show();
-//
-//        return false;
-//    }
-//    return false;
-//}
+	// Get la liste des competences selon domaine d'intervention	
+
 jQuery(document).ready(function (e) {
     
     
@@ -66,68 +49,76 @@ jQuery(document).ready(function (e) {
         jQuery("#callme").live('click', function(){
             jQuery('.info-bloc #popup_overlay2 span.required').hide();
             jQuery('.info-bloc #popup_overlay2').show(); 
-            jQuery('.info-bloc #popup_overlay2 #edit-actions').show(); 
-
         });
          jQuery(".info-bloc #popup_overlay2 .close").live('click', function(){
-
             jQuery('.info-bloc #popup_overlay2').hide(); 
             jQuery('.info-bloc #popup_overlay2 #edit-actions').hide(); 
-
         });
         jQuery('#pccontact_popup_form').submit(function () {
-              
-            var name = jQuery('#pccontact_popup_form').find('#edit-nom').val();
-            var prenom = jQuery('#pccontact_popup_form').find('#edit-prenom').val();
+  
             var phone = jQuery('#pccontact_popup_form').find('#edit-telephone').val();
             var email = jQuery('#pccontact_popup_form').find('#edit-email').val();
        
-      
             var emailReg = /^^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/i;
             var phoneReg = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
-		
-            errorInput('edit-nom');
-            errorInput('edit-prenom');
-            errorInput('edit-telephone');
-            errorInput('edit-email');
+            	
+            nameRes =  errorInput('edit-nom');
+            prenomRes = errorInput('edit-prenom');
+            telRes = errorInput('edit-telephone');
+            emailRes =  errorInput('edit-email');
 
-            if (name === "" || prenom === "" || phone === "" || email === "") {
+            if (!nameRes  || !prenomRes || !telRes || !emailRes) {
                     var GlobalError = jQuery('#pccontact_popup_form').attr('data-GlobalError');
                      jQuery(document).find('.errorForms').remove();
-                     jQuery(document).find('#pccontact_popup_form #edit-actions')
-                            .append('<p class="errorForms">'+GlobalError+'</p>').hide().fadeIn().show();
-                    return false;
+                     jQuery(document).find('#pccontact_popup_form #edit-actions').append('<p class="errorForms">'+GlobalError+'</p>').hide().fadeIn().show();
+ 
+            }else if (!phoneReg.test( phone ) ) {                                   
+                    jQuery('#edit-telephone').addClass('error'); 
+                    jQuery('.edit-telephone span.required').show();
+            }else if ( !emailReg.test( email ) ) {                                     
+                    jQuery('#edit-email').addClass('error');  
+                    jQuery('.edit-email span.required').show();
             }else{
+                        jQuery.ajax({			
+                        type: "GET",
+                        url:Drupal.settings.basePath+'popup/ajax',
+                        dataType: 'json',	
+                        data: 'nom='+jQuery('#edit-nom').val()+'&prenom='+jQuery('#edit-prenom').val()+'&phone='+jQuery('#edit-telephone').val()+'&email='+jQuery('#edit-email').val(),
+                        contentType: "application/json",
+                        success: function(res) {		
+                               if(res.data){
+                                   
+                                    jQuery('#popupContent').hide(); 
+                                    jQuery('.info-bloc #popup_overlay2 #edit-actions').hide();
+                                    jQuery('.info-bloc #popup_overlay2 #messageSent').show();
+  
+                               }else{   
                         
-                                               
-                        jQuery('#popupContent').hide(); 
-                        jQuery('.info-bloc #popup_overlay2 #edit-actions').hide(); 
-            		jQuery('.info-bloc #popup_overlay2 #messageSent').show();
-                   return false;       
-            }			
+                                    jQuery(document).find('.errorForms').remove();
+                                    jQuery(document).find('#pccontact_popup_form #edit-actions').append('<p class="errorForms">Erreur d\'envoi, merci de r&#233;essayer ult&#233;rieurement</p>').hide().fadeIn().show();                                  
+                               }
+                        },
+                        error: function(xhr, status) {  			
+                               alert('Unknown ' + status); 					
+                        }
+                        });
+                        
+                       
+ 
+            }	
+               return false;  
     });
     
      var errorInput = function (id){
+ 
         if( jQuery("#"+id).val() === ""){            
             jQuery("#"+id).addClass('error');
             jQuery("."+id+' span.required').show();
-        }else if ( !emailReg.test( email ) ) {
-//                    var MailError = jQuery('#pccontact_general_form').find('#edit-email').attr('data-MailError');
-//                    jQuery(document).find('.errorForms').remove();
-//                    jQuery(document).find('#pccontact_general_form #edit-actions')
-//                            .append('<p class="errorForms">'+MailError+'</p>').hide().fadeIn().show();
-                    jQuery('#edit-email').addClass('error');   
-                return false;
-            }else if (!phoneReg.test( phone ) ) {
-//			var PhoneError = jQuery('#pccontact_general_form').find('#edit-phone').attr('data-PhoneError');
-//			jQuery(document).find('.errorForms').remove();
-//			jQuery(document).find('#pccontact_general_form #edit-actions')
-//				.append('<p class="errorForms"> '+PhoneError+'</p>').hide().fadeIn().show();
-                        jQuery('#edit-telephone').addClass('error');   
-                return false;
-            }else{                 
+            return false;
+        }else{                 
             jQuery("#"+id).removeClass('error');
             jQuery("."+id+' span.required').hide();
+            return true;
         }
     };
     

1ff812de82ef0ed2f0aacdafd6072019ba6edf7f
opsagenceoneo <opsa@genceoneo.com>
2014-09-05 11:15:16 +0000

    ignore file

---
+++ sites/default/global_settings.php
@@ -0,0 +1,1 @@
+<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_prd_22072014','username'=>'root','password'=>'xedr386','host'=>'localhost','port'=>'','prefix'=>'',),),);

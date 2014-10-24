e7e79a6a54951ba71872f082134a086f4912676d
opsagenceoneo <support@agenceoneo.com>
2014-10-24 15:01:44 +0000

    fix bug calendrier

--- sites/default/global_settings.php
+++ sites/default/global_settings.php
@@ -1,1 +1,1 @@
-<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_09102014','username'=>'root','password'=>'xedr386','host'=>'localhost','port'=>'','prefix'=>'',),),);
+<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_23102014','username'=>'root','password'=>'xedr386','host'=>'localhost','port'=>'','prefix'=>'',),),);

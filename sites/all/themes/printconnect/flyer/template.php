08170e3e261b63874f5f4f8e905a33d0cf24c7e0
opsagenceoneo <ops@agenceoneo.com>
2014-10-31 19:18:01 +0100

    correct designtool popup bug

--- sites/all/themes/printconnect/flyer/template.php
+++ sites/all/themes/printconnect/flyer/template.php
@@ -233,6 +233,7 @@ function flyer_preprocess_html(&$vars) {
 }
 
  function flyer_js_alter(&$javascript) {
+	if (arg(0) != 'cart')
 unset($javascript['sites/all/themes/printconnect/flyer/libraries/jquery.selectBox/jquery.selectBox.js']);
 	if ($_SESSION['isfront'] == 1) {
             unset($javascript['sites/all/modules/printconnect/pcbpost/pcbpost.js']);

94f4140a29a42c9eabfc398b11ede3766b3911d7
ghazali mustapha <mustapha.ghazali@printconcept.com>
2015-03-24 13:02:48 +0000

    code designer

--- sites/all/modules/printconnect/pcdesigner/pcdesigner.js
+++ sites/all/modules/printconnect/pcdesigner/pcdesigner.js
@@ -5,13 +5,34 @@
 
     },
     attach: function (context, settings) {
-      $('input[name="cart[designer][email]"]').change(function(){
+      $('#pccart-cart-form .inputdesigneremail').change(function(){
         var href = "js/upload-design/designer/email/" + $(this).val(); 
         alert (href);
         $.getJSON(href, function(data){          
           
         })
       })
+   
+       if ($("#pccart-cart-form .inputdesigner")[0]){
+         $(".inputdesigner ").hide();
+        }  
+     
+     
+       $('#pccart-cart-form .form-checkbox').change(function(){
+           console.log('yes');
+      if ($(this).is(':checked')) {
+               $(this).parent().siblings(".inputdesigner").show();
+               $(this).parent().siblings(".prodactTemplates").hide();
+            } else {
+               $(this).parent().siblings(".inputdesigner").hide();
+                  $(this).parent().siblings(".prodactTemplates").show();
+            }
+      })
+
+            
+
+     
+      
     }
   }
 })(jQuery);

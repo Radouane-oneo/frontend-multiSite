ae27db58a093c67830dba785e20de1a673dbc2de
Ayoub BAHRA <ayoub.b@oneoweb.com>
2015-04-20 16:48:16 +0100

    disable decode json

--- configSelector/js/config.js
+++ configSelector/js/config.js
@@ -2,6 +2,9 @@ define([
     'backbone',
 ], function (Backbone) {
     var configJSON = JsonFileGroups;
+    GlobalLabels['productId'] = JsonFileGroups.id;
+    GlobalLabels['productName'] = JsonFileGroups.name;
+    GlobalLabels['productDescription'] = JsonFileGroups.shortDescription + JsonFileGroups.longDescription + JsonFileGroups.baselineDescription;
     return {
         themeFile : "productConfig-flyer.html",
         containerId : "#myForm",

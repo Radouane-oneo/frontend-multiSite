7ed01b804ffdb21fcaa8c73c8954b4bec5cc9bd9
Loubna <loubna.fei@loubna.fei@oneoweb.com>
2015-06-10 17:03:33 +0100

    correct error

--- sites/all/modules/printconnect/pccart/pccart.js
+++ sites/all/modules/printconnect/pccart/pccart.js
@@ -124,7 +124,11 @@ function PriceCallback() {
 	});
 	map.forEach(function(Price) { totalprice += Price; });
 	jQuery("#pccart-cart-form .subtotal .value").html(buildPriceHtml(totalprice, false));
-	var vat = 0.21 ;
+            try{
+          var vat = Drupal.settings.pccart.VatCart;
+         }catch(e){
+         }
+        vat = parseFloat(vat);
 	vatprice = totalprice * vat ;
 	jQuery("#pccart-cart-form .vat .value").html(buildPriceHtml(vatprice, false));
 	total = vatprice + totalprice;

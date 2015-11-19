8d9f21b89e5c37ed6618cc41af1511eb58b66972
root <root@ip-10-120-11-73.eu-west-1.compute.internal>
2015-11-19 09:53:04 +0000

    fix bug die after order mise en page

--- sites/default/global_settings.php
+++ sites/default/global_settings.php
@@ -1,1 +1,1 @@
-<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_fr_15092015','username'=>'root','password'=>'xedr386','host'=>'localhost','port'=>'','prefix'=>'',),),);
+<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_fr_17112015','username'=>'root','password'=>'q6BsexDwGuUeuYWa','host'=>'localhost','port'=>'','prefix'=>'',),),);

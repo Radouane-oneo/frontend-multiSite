3f1a652fd2b564e74e5ce74a45e89321a51d9fe6
root <root@ip-10-120-11-73.eu-west-1.compute.internal>
2015-12-01 10:49:20 +0000

    time out on dal

--- sites/default/global_settings.php
+++ sites/default/global_settings.php
@@ -1,1 +1,1 @@
-<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_fr_25112015','username'=>'root','password'=>'q6BsexDwGuUeuYWa','host'=>'localhost','port'=>'','prefix'=>'',),),);
+<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_fr_30112015','username'=>'root','password'=>'q6BsexDwGuUeuYWa','host'=>'localhost','port'=>'','prefix'=>'',),),);

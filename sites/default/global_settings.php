09739e20a0268b59985b6803aca1a3cd89d3243b
root <root@ip-10-120-11-73.eu-west-1.compute.internal>
2015-12-02 16:52:04 +0000

    setting

--- sites/default/global_settings.php
+++ sites/default/global_settings.php
@@ -1,1 +1,1 @@
-<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_fr_30112015','username'=>'root','password'=>'q6BsexDwGuUeuYWa','host'=>'localhost','port'=>'','prefix'=>'',),),);
+<?php $databases = array('default'=>array('default'=>array('driver'=>'mysql','database'=>'flyer_fr_01122015','username'=>'root','password'=>'q6BsexDwGuUeuYWa','host'=>'localhost','port'=>'','prefix'=>'',),),);

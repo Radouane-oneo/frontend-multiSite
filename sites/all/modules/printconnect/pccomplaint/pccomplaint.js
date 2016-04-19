c5779fa1f565149b44fc123fc2eb8da190a8df2d
oneosaji <saji.m@oneoweb.com>
2016-04-19 11:04:45 +0100

    Complaint JS validation

--- sites/all/modules/printconnect/pccomplaint/pccomplaint.js
+++ sites/all/modules/printconnect/pccomplaint/pccomplaint.js
@@ -3,3 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+(function ($){
+	document.ready(function () {
+		// Verification JS pour le formulaire complaint.
+		jQuery('.complaintSubmit input.ui-button').click(function (){
+			console.log('coco');
+			jQuery('#content .complaintform .required').removeClass("error");
+			jQuery(".errorMsg").hide();
+
+			jQuery('#content .complaintform .required').each(function(i, elem) {
+				var _this = $(this); 
+
+				if(_this.val() == "" || _this.val() == 0) {
+					e.preventDefault();
+
+					_this.addClass("error");
+					_this.siblings(".errorMsg").show();
+				};
+			})
+			
+		});
+
+	});
+})(jQuery);

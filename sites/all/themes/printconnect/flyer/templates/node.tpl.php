fc3ab1ddcdd6c37b320dbd655f4b1450783f68ae
mohammed <mohammed@oneo.dev>
2016-05-12 11:18:31 +0200

    twitter cards

--- sites/all/themes/printconnect/flyer/templates/node.tpl.php
+++ sites/all/themes/printconnect/flyer/templates/node.tpl.php
@@ -1,3 +1,84 @@
+<?php 
+	
+	/*
+	//header('Content-Type: application/json');
+	
+	foreach ($node->body as $key => $value) {
+		$desc = $value[0]['value'];
+	}
+	
+	$description = substr(preg_replace('/\s+/', ' ', strip_tags(preg_replace("/<script\\b[^>]*>(.*?)<\\/script>/s", "",preg_replace("/<style\\b[^>]*>(.*?)<\\/style>/s", "", print_r($desc, 1))))), 0, 600) . '...';
+	
+	$nodejson = json_encode($node);
+	
+	$node = json_decode($nodejson, true); 
+	print $description . '<br><br>';
+	
+	//print indent(json_encode($node));
+	$parentnode =  $node['view']['display']['default']['display_options']['title'];
+	
+	print $parentnode . '<br><br>';
+	
+	$title = $node['title'];
+	
+	print $title . '<br><br>';
+	//print_r($node);
+	
+	die(); */
+	
+	function indent($json) {
+
+	    $result      = '';
+	    $pos         = 0;
+	    $strLen      = strlen($json);
+	    $indentStr   = '  ';
+	    $newLine     = "\n";
+	    $prevChar    = '';
+	    $outOfQuotes = true;
+	
+	    for ($i=0; $i<=$strLen; $i++) {
+	
+	        // Grab the next character in the string.
+	        $char = substr($json, $i, 1);
+	
+	        // Are we inside a quoted string?
+	        if ($char == '"' && $prevChar != '\\') {
+	            $outOfQuotes = !$outOfQuotes;
+	
+	        // If this character is the end of an element,
+	        // output a new line and indent the next line.
+	        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+	            $result .= $newLine;
+	            $pos --;
+	            for ($j=0; $j<$pos; $j++) {
+	                $result .= $indentStr;
+	            }
+	        }
+	
+	        // Add the character to the result string.
+	        $result .= $char;
+	
+	        // If the last character was the beginning of an element,
+	        // output a new line and indent the next line.
+	        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+	            $result .= $newLine;
+	            if ($char == '{' || $char == '[') {
+	                $pos ++;
+	            }
+	
+	            for ($j = 0; $j < $pos; $j++) {
+	                $result .= $indentStr;
+	            }
+	        }
+	
+	        $prevChar = $char;
+	    }
+	
+	    return $result;
+	}
+	
+?>
+
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
     <div class="content"<?php print $content_attributes; ?>>
        <?php

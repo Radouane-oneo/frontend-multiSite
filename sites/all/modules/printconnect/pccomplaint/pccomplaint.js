3ac3f5485b58d82ec62362bb0cbdd1b7c314c3c5
loubna <louna.fei@oneodev.com>
2016-05-17 17:00:20 +0200

    correct

--- sites/all/modules/printconnect/pccomplaint/pccomplaint.js
+++ sites/all/modules/printconnect/pccomplaint/pccomplaint.js
@@ -113,25 +113,61 @@
             jQuery('#box-progress').show();
             jQuery.ajax({           
                 url: href,
+//                success: function(data){
+//                    if (data == 'false') 
+//                    {   console.log('NotValidOrder');
+//                        displayerror();
+//                    }
+//                    else
+//                    {   console.log('ordervalid');
+//                        jQuery('#box-progress').hide();
+//                        jQuery("#errorMsg").css({ "display":"none"});
+//                        jQuery("#edit-orderid").removeClass('error');
+//                        if(Dropzone.instances[0]) Dropzone.instances[0].enable();
+//                        console.log('success');
+//                        jQuery("#errorUpload").css({ "display":"none"});
+//                        if (action == 'submit'){
+//                            jQuery("#pccomplaint-form").submit();
+//                            jQuery("#pccomplaint-form .complaintSubmit").css({ "display":"none"});
+//                            jQuery(".complaintSuccess").css({ "display":"block"});
+//                        }
+//                }
+//                },
                 success: function(data){
-                    if (data == 'false') 
-                    {   console.log('NotValidOrder');
+                    
+                    if (!data.id) 
+                    {                          
                         displayerror();
                     }
                     else
-                    {   console.log('ordervalid');
-                        jQuery('#box-progress').hide();
-                        jQuery("#errorMsg").css({ "display":"none"});
-                        jQuery("#edit-orderid").removeClass('error');
-                        if(Dropzone.instances[0]) Dropzone.instances[0].enable();
-                        console.log('success');
-                        jQuery("#errorUpload").css({ "display":"none"});
-                        if (action == 'submit'){
-                            jQuery("#pccomplaint-form").submit();
-                            jQuery("#pccomplaint-form .complaintSubmit").css({ "display":"none"});
-                            jQuery(".complaintSuccess").css({ "display":"block"});
+                    {
+                        if(data.orderStatus && [10, 18, 15].indexOf(data.orderStatus.id)!= -1){
+                            console.log(data.orderStatus.id);
+                            if(Dropzone.instances[0]) Dropzone.instances[0].enable();
+                            jQuery("#errorUpload").css({ "display":"none"});
+                            jQuery('#box-progress').hide();
+                            jQuery("#errorMsg").css({ "display":"none"});
+                            jQuery('#edit-orderid').parent().find('.errorMsg').remove();
+                            jQuery("#errorMsg").css({ "display":"none"});
+                            jQuery("#edit-orderid").removeClass('error');                            
+                            jQuery.each(data.orderItems, function(i,orderitem) {                                
+                                if (!orderitem.discountId)
+                                {
+                                  jQuery("#edit-jobid").append(jQuery("<option>").attr("value", orderitem.id).attr("data-box", orderitem.tracking.length).text(orderitem.id));
+                                }
+                             });
+                            jQuery('.form-item-jobid .select2-chosen').html(data.orderItems[0].id);
+                            jQuery("#s2id_edit-jobid .select2-chosen").find('option:eq(0)').prop('selected',true);
+                            if (action == 'submit'){
+                                jQuery("#pccomplaint-form").submit();
+                                jQuery("#pccomplaint-form .complaintSubmit").css({ "display":"none"});
+                                jQuery(".complaintSuccess").css({ "display":"block"});
+                            }                            
                         }
-                }
+                        else{
+                            displayerror();
+                        }
+                    }
                 },
                 error: function(XMLHttpRequest, textStatus, errorThrown) {
                     jQuery('#box-progress').hide();

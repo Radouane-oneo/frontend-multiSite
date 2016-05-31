2b69bc678251d96d3ad94e80582f9ce85c329d65
Mohamed Benatti <benatti.med@gmail.com>
2016-05-31 16:47:50 +0200

    twitter card implementation

--- sites/all/themes/printconnect/flyer/templates/node.tpl.php
+++ sites/all/themes/printconnect/flyer/templates/node.tpl.php
@@ -1,82 +1,110 @@
 <?php 
-	
-	/*
 	//header('Content-Type: application/json');
+	//print '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<';
+	$title       = $content['body']['#object']->title;
+	$description = $content['body']['#object']->field_description_meta["und"][0]["metatags_quick"];
+	
+	$body = $content['body']['#object']->body;
 	
 	foreach ($node->body as $key => $value) {
 		$desc = $value[0]['value'];
 	}
-	
+	if(empty($description))
 	$description = substr(preg_replace('/\s+/', ' ', strip_tags(preg_replace("/<script\\b[^>]*>(.*?)<\\/script>/s", "",preg_replace("/<style\\b[^>]*>(.*?)<\\/style>/s", "", print_r($desc, 1))))), 0, 600) . '...';
 	
-	$nodejson = json_encode($node);
+	if(!empty($description) && $description != '...') {
+		$body = stripslashes(json_encode($body));
 	
-	$node = json_decode($nodejson, true); 
-	print $description . '<br><br>';
-	
-	//print indent(json_encode($node));
-	$parentnode =  $node['view']['display']['default']['display_options']['title'];
-	
-	print $parentnode . '<br><br>';
-	
-	$title = $node['title'];
-	
-	print $title . '<br><br>';
-	//print_r($node);
-	
-	die(); */
-	
-	function indent($json) {
-
-	    $result      = '';
-	    $pos         = 0;
-	    $strLen      = strlen($json);
-	    $indentStr   = '  ';
-	    $newLine     = "\n";
-	    $prevChar    = '';
-	    $outOfQuotes = true;
-	
-	    for ($i=0; $i<=$strLen; $i++) {
-	
-	        // Grab the next character in the string.
-	        $char = substr($json, $i, 1);
-	
-	        // Are we inside a quoted string?
-	        if ($char == '"' && $prevChar != '\\') {
-	            $outOfQuotes = !$outOfQuotes;
-	
-	        // If this character is the end of an element,
-	        // output a new line and indent the next line.
-	        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-	            $result .= $newLine;
-	            $pos --;
-	            for ($j=0; $j<$pos; $j++) {
-	                $result .= $indentStr;
-	            }
-	        }
-	
-	        // Add the character to the result string.
-	        $result .= $char;
-	
-	        // If the last character was the beginning of an element,
-	        // output a new line and indent the next line.
-	        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-	            $result .= $newLine;
-	            if ($char == '{' || $char == '[') {
-	                $pos ++;
-	            }
-	
-	            for ($j = 0; $j < $pos; $j++) {
-	                $result .= $indentStr;
-	            }
-	        }
-	
-	        $prevChar = $char;
-	    }
-	
-	    return $result;
+		$imgposition = strpos($body, 'header-image', 0);
+		$imgposition2 = strpos($body, 'header--image', 0);
+		
+		$headerimage = strpos(render($content), '.headerimage', 0);
+		
+		$topimg = strpos(render($content), '.topimg', 0);
+		
+		$summaryimg = strpos(render($content), 'field-type-text-with-summary', 0);
+		$summaryimg = strpos(render($content), '<img', $summaryimg);
+		
+		if($imgposition) {
+			$first = strpos($body, 'src="', $imgposition);
+			$last  = strpos($body, '"', $first+5);
+			
+			$src   = substr($body, $first+5, ($last - $first) - 5);
+		} else if($imgposition2) {
+			$first = strpos($body, 'src="', $imgposition2);
+			$last  = strpos($body, '"', $first+5);
+			
+			$src   = substr($body, $first+5, ($last - $first) - 5);
+		} else if($headerimage) {
+			$first = strpos(render($content), "url(", $headerimage);
+			$last  = strpos(render($content), ")", $first+4);
+			
+			$src   = substr(render($content), $first+4, ($last - $first) - 4);
+		} else if($topimg) {
+			$first = strpos(render($content), "url(", $topimg);
+			$last  = strpos(render($content), ")", $first+4);
+			
+			$src   = substr(render($content), $first+4, ($last - $first) - 4);
+		} else if($summaryimg) {
+			$first = strpos(render($content), 'src="', $summaryimg);
+			$last  = strpos(render($content), '"', $first+5);
+			
+			$src   = substr(render($content), $first+5, ($last - $first) - 5);
+		}
+		
+		$type = 'summary';
+		$element = array(
+		  '#tag' => 'meta', // The #tag is the html tag - 
+		  '#attributes' => array( // Set up an array of attributes inside the tag
+		    'name' => 'twitter:title', 
+		    'content' => strtoupper ( trim($title) )
+		  ),
+		);
+		drupal_add_html_head($element, 'twitter_title');
+		
+		$element = array(
+		  '#tag' => 'meta', // The #tag is the html tag - 
+		  '#attributes' => array( // Set up an array of attributes inside the tag
+		    'name' => 'twitter:description', 
+		    'content' => trim($description)
+		  ),
+		);
+		drupal_add_html_head($element, 'twitter_description');
+		
+		if(strpos($src, 'http://') != -1) {
+			$img  = str_replace('"', '', trim($src));
+			
+			$element = array(
+			  '#tag' => 'meta', // The #tag is the html tag - 
+			  '#attributes' => array( // Set up an array of attributes inside the tag
+			    'name' => 'twitter:image', 
+			    'content' => $img
+			  ),
+			);
+			drupal_add_html_head($element, 'twitter_image');
+			
+			$size = getimagesize($img);
+			
+			if($size[0] > 500) {
+				$type = 'summary_large_image';
+			}
+			//print '<script> console.log(\'image\', \'' .. '\')</script>';
+		}
+		
+		$element = array(
+		  '#tag' => 'meta', // The #tag is the html tag - 
+		  '#attributes' => array( // Set up an array of attributes inside the tag
+		    'name' => 'twitter:card', 
+		    'content' => $type
+		  ),
+		);
+		drupal_add_html_head($element, 'twitter_type');
+		
 	}
 	
+	
+	//print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
+	//die();
 ?>
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
